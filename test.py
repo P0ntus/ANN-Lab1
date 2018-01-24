99390a602a2436b9d43c41915ce3ecfb7f229037
Pontus Tobiasson <pontob@kth.se>
2018-01-24 11:59:17 +0100

    First commit!

---
+++ test.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import shuffle
+
+class MyClass:
+    def __init__(self, x, y, c):
+        self.x = x
+	self.y = y
+	self.classification = c
+
+meanA = [3, 3]
+meanB = [-3, -3]
+cov = [[1, 0], [-5, 10]]  # diagonal covariance
+
+points = []
+
+Class_A = np.random.multivariate_normal(meanA, cov, 100).T
+for x in range(0, 100):
+    points.append(MyClass(Class_A[0][x], Class_A[1][x], 1))
+
+Class_B = np.random.multivariate_normal(meanB, cov, 100).T
+for x in range(0, 100):
+    points.append(MyClass(Class_B[0][x], Class_B[1][x], -1))
+
+shuffle(points)
+
+
+input_pattern = []
+output_pattern = []
+target_pattern = []
+
+for point in points:
+    input_pattern.append([point.x, point.y, 1])
+    target_pattern.append(point.classification)
+
+np.transpose(input_pattern)
+
+#for x in range(0, 200):
+ #   print(input_pattern[x][0], input_pattern[x][1], input_pattern[x][2], target_pattern[x])
+
+for x in range(0, 200):
+    print(input_pattern[x][0])
+
+
+
+plt.plot(input_pattern[:2], input_pattern[:2], 'x')
+plt.axis('equal')
+plt.show()
+
+#plt.plot(x_a, y_a, 'x')
+#plt.plot(x_b, y_b, 'o')
+#plt.axis('equal')
+#plt.show()
+#print(x_a)
+#print(x_a[0][0])
+
+

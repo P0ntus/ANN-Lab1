9fb2ca00c066d9a2b87ec9adf7044004b9f7fe2e
Pontus Tobiasson <pontob@kth.se>
2018-01-25 12:27:24 +0100

    x

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from random import shuffle
@@ -18,18 +19,15 @@ meanB = [-3, -3]
 cov = [[1, 0], [-5, 10]]  # diagonal covariance
 points = []
 
-
 Class_A = np.random.multivariate_normal(meanA, cov, NUM_POINTS/2).T
 for x in range(0, NUM_POINTS/2):
     points.append(Point(Class_A[0][x], Class_A[1][x], 1))
-
 
 Class_B = np.random.multivariate_normal(meanB, cov, NUM_POINTS/2).T
 for x in range(0, NUM_POINTS/2):
     points.append(Point(Class_B[0][x], Class_B[1][x], -1))
 
 shuffle(points)
-
 
 # Initialize input output and target pattern based on earlier generated points
 input_pattern = []
@@ -42,7 +40,7 @@ for point in points:
 input_pattern = np.transpose(input_pattern)
 
 
-# Perceptron Learning
+# Initiate weights using small random numbers drawn from the normal distribution with zero mean
 '''
 weight = []
 mu, sigma = 0, 0.1
@@ -50,6 +48,10 @@ weight.append(np.random.normal(mu, sigma, 1)[0])
 weight.append(np.random.normal(mu, sigma, 1)[0])
 weight.append(np.random.normal(mu, sigma, 1)[0])
 learning_rate = 0.00001
+'''
+
+'''
+# Perceptron Learning
 error = []
 for x in range(0, 1000):
     output_pattern = np.dot(np.transpose(weight), input_pattern)
@@ -63,22 +65,60 @@ for x in range(0, 1000):
     #print(np.sum(error))
 '''
 
-
+'''
 # Delta Learning
-weight = []
-mu, sigma = 0, 0.1
-weight.append(np.random.normal(mu, sigma, 1)[0])
-weight.append(np.random.normal(mu, sigma, 1)[0])
-weight.append(np.random.normal(mu, sigma, 1)[0])
-learning_rate = 0.00001
 error = []
 for x in range(0, 1000):
     output_pattern = np.dot(np.transpose(weight), input_pattern)
     error = np.subtract(target_pattern, output_pattern)
     weight += np.dot(learning_rate, np.dot(error, np.transpose(input_pattern)))
     print(np.sum(error))
-    
+'''
 
+
+def phi( h_input ):
+   h_output = np.divide(2 , ( 1 + np.exp( - h_input ) ) ) - 1
+   return h_output
+
+# Two-layer perceptron
+NUM_NODES = 4
+NUM_ITERATIONS = 10
+STEP_LENGTH = 2
+v_size = 4
+w_size = 3
+
+# Initiate weights using small random numbers drawn from the normal distribution with zero mean
+mu, sigma = 0, 0.1
+
+v = []
+#print( input_pattern )
+for x in range(0, v_size):
+	v.append(np.random.normal(mu, sigma, NUM_POINTS))
+
+w = []
+for x in range(0, w_size):
+	w.append(np.random.normal(mu, sigma, v_size))
+
+# print(v)
+
+# print(w)
+#print (input_pattern)
+#print (np.transpose(input_pattern))
+# Forward pass
+h_input = np.dot(v, np.transpose(input_pattern))
+print(h_input)
+h_output = phi( h_input )
+print(h_output)
+
+o_input = np.dot(w, h_output)
+o_output = phi( o_input )
+#print(o_output)
+
+delta_o = (o_output - np.asarray(target_pattern)) * ((1 + o_output) * (1 - o_output)) * 0.5
+#print(delta_o)
+
+
+'''
 # Plot the points
 positive = []
 negative = []
@@ -102,6 +142,7 @@ y2 = - (weight[0] / weight[1]) * 15 - (weight[2] / weight[1])
 plt.plot([-15, 15], [y1, y2])
 plt.axis('equal')
 plt.show()
+'''
 
 
 

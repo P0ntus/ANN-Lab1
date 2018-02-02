2ae43fd098f6374ffd71e9956f847c620099a7fd
Pontus Tobiasson <pontob@kth.se>
2018-02-02 14:14:00 +0100

    splitting of datasets

--- Mackey-Glass-sklearn.py
+++ Mackey-Glass-sklearn.py
@@ -40,18 +40,23 @@ for t in range(OFFSET+50, POINTS_TO_GEN + 50):
 	target_pattern.append(prev_x[t])
 
 # Split into training and test set
-training_target_pattern = target_pattern[0:TRAINING_SIZE]
-test_target_pattern = target_pattern[TRAINING_SIZE:NUM_POINTS]
-
 training_input_pattern = input_pattern[0:TRAINING_SIZE]
 test_input_pattern = input_pattern[TRAINING_SIZE:NUM_POINTS]
 
-input_pattern = np.array(input_pattern)
-#print(len(target_pattern))
+training_target_pattern = target_pattern[0:TRAINING_SIZE]
+test_target_pattern = target_pattern[TRAINING_SIZE:NUM_POINTS]
+
+training_input_pattern = np.array(training_input_pattern)
+test_input_pattern = np.array(test_input_pattern)
+
 #input_pattern = (input_pattern - input_pattern.mean(axis=0))/input_pattern.var(axis=0)
-input_pattern = np.transpose(input_pattern)
-target_pattern = np.array(target_pattern)
-target_pattern = (target_pattern - target_pattern.mean(axis=0))/target_pattern.std(axis=0) / 2.5
+training_input_pattern = np.transpose(training_input_pattern)
+test_input_pattern = np.transpose(test_input_pattern)
+
+training_target_pattern = np.array(training_target_pattern)
+training_target_pattern = (training_target_pattern - training_target_pattern.mean(axis=0))/training_target_pattern.std(axis=0) / 2.5
+test_target_pattern = np.array(test_target_pattern)
+test_target_pattern = (test_target_pattern - test_target_pattern.mean(axis=0))/test_target_pattern.std(axis=0) / 2.5
 
 #for t in range(0, 5):
 	#input_pattern[t] = normalize(input_pattern[t])
@@ -63,12 +68,12 @@ output_size = 1
 hidden_layer_size = 15
 
 reg = MLPRegressor(hidden_layer_sizes=hidden_layer_size)
-reg = reg.fit(np.transpose(input_pattern), target_pattern)
-output = reg.predict(np.transpose(input_pattern))
+reg = reg.fit(np.transpose(training_input_pattern), training_target_pattern)
+output = reg.predict(np.transpose(training_input_pattern))
 
 '''
 plt.plot(output)
-plt.plot(target_pattern)
+plt.plot(training_target_pattern)
 plt.legend(['y = ANN', 'y = TARGET'])
 plt.ylabel('some numbers')
 plt.show()
